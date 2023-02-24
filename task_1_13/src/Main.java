2273b1fd0f2127346a7a602b11465b9a338d04c9
k0te1ch <khvostov40@gmail.com>
2023-02-25 01:15:57 +0300

    Initial commit

---
+++ task_1_13/src/Main.java
@@ -0,0 +1,16 @@
+/*
+1-13
+Реализовать класс Формула (интерпретатор). Должна быть возможность работы с
+данным классом следующим образом:
+ Formula f = new Formula();
+ f.prepare("x*x +y");
+ double v = f.execute(2, 5.7);
+ // и т.п.
+*/
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
